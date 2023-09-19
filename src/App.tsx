d273cced9c3996cddfc7cab81694a25dc487b75b
vkirillov <blend2012@yandex.ru>
2023-09-19 17:46:56 +0300

    Init firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
 import { useState } from 'react';
 
+import { initializeApp } from 'firebase/app';
+// import { getAnalytics } from 'firebase/analytics';
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyDf6EmJoSfqMXI1bbvXHPIDh0flGyhtrsw',
+  authDomain: 'giftbox-af946.firebaseapp.com',
+  projectId: 'giftbox-af946',
+  storageBucket: 'giftbox-af946.appspot.com',
+  messagingSenderId: '346863241139',
+  appId: '1:346863241139:web:98437796089c9e3d50edc6',
+  //measurementId: 'G-ELRJEX9V9Z',
+};
+
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+console.log(app);
+
 function App() {
   const [count, setCount] = useState(0);
 
