b52893a43532715fe90ee072bed7ba8f642275ac
vkirillov <blend2012@yandex.ru>
2025-02-22 16:22:37 +0300

    continue remove old logic

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,21 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 
-import { IncomesSlice } from './incomesState';
-import { ExpensesSlice } from './expensesState';
-import { StoragesSlice } from './storagesState';
-import { UserSlice } from './user';
-import { AvailableCurrenciesSlice } from './availableCurrencies';
-import { CurrencyRatesSlice } from './currencyRatesState';
 import { SpreadsheetListSlice } from './spreadsheetList';
 
 export const store = configureStore({
   reducer: {
-    user: UserSlice.reducer,
-    incomes: IncomesSlice.reducer,
-    expenses: ExpensesSlice.reducer,
-    storages: StoragesSlice.reducer,
-    availableCurrencies: AvailableCurrenciesSlice.reducer,
-    currencyRates: CurrencyRatesSlice.reducer,
     spreadsheetList: SpreadsheetListSlice.reducer,
   },
 });
